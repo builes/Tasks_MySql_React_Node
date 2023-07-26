28d955205d03a2e743b65edb621a4f362e85802b
Mauricio Builes <rmbuilesm@unal.edu.co>
2023-07-26 16:11:54 -0500

    done the build to react and configured node to use static files

--- back/db.js
+++ back/db.js
@@ -1,9 +1,10 @@
 import { createPool } from 'mysql2/promise';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
 // createPool() crea una conexión a la base de datos, la bd debe estar creada
 export const pool = createPool({
-	host: 'localhost',
-	user: 'root',
-	password: '1234',
-	database: 'task_db',
+	host: DB_HOST,
+	user: DB_USER,
+	password: DB_PASSWORD,
+	database: DB_NAME,
 });
--- back/index.js
+++ back/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import indexRoutes from './routes/indexRoutes.js';
 import taskRoutes from './routes/tasksRoutes.js';
 
 import { PORT } from './config.js';
 
 const app = express();
+
+// __dirname nos dice en que directorio estamos trabajando
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // cors es un middleware que nos permite hacer peticiones desde un servidor a otro
 app.use(cors());
@@ -19,6 +24,9 @@ app.use(morgan('dev'));
 // y poder interpretarlos en el servidor como un objeto de javascript
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Con esto le decimos a express que la carpeta public es de acceso publico
+app.use(express.static(join(__dirname, '../front/dist')));
 
 // routes
 app.use(indexRoutes);
